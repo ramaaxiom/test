35a370e9293586829960656ce0dde2edcbef1ba2
Rama Alapati <ramk.alapati@gmail.com>
2013-10-22 16:44:31 +0530

    mytest

---
+++ Test1_project/CoreImage/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreImage
+//
+//  Created by  on 11-11-13.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
